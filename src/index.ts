ddaf0fb9f5699094b70d40919c5180a3cbde940a
juanlarrambebere <juan@streaver.com>
2022-05-31 18:24:03 -0300

    Installed dotenv, express and setup a basic express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("HELLO WORLD!");
+import "dotenv/config";
+import express from "express";
+
+const PORT = process.env.APP_PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`listening on port http://localhost:${PORT}`);
+});
